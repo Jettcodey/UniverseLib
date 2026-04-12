37a859961199a006847d8ca890c3e84859b87086
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-04-13 08:24:18 +0900

    feat: ptr extentions by HerpDerpinstine

---
+++ src/Utility/Il2CppPointerHelper.cs
@@ -0,0 +1,27 @@
+﻿#if CPP
+using System;
+#if INTEROP
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.InteropTypes;
+#else
+using UnhollowerBaseLib;
+#endif
+
+namespace UniverseLib.Utility;
+
+
+public static class Il2CppPointerHelper
+{
+    /// <summary>
+    /// Returns the Pointer to any given Il2Cpp Object.
+    /// </summary>
+    internal static IntPtr ToIl2CppPointer<T>(this T obj)
+        where T : Il2CppObjectBase
+    {
+        // Get Pointer from Unhollower/Il2CppInterop instead of .Pointer
+        // This ensures greater compatibility with any variation in behavior
+        return IL2CPP.Il2CppObjectBaseToPtr(obj);
+    }
+}
+
+#endif
